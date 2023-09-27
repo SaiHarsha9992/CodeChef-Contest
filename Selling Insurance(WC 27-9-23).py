69c1073a4a24c799268dc549f82494af048b1e20
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2023-09-27 22:30:22 +0530

    Add files via upload

---
+++ Selling Insurance(WC 27-9-23).py
@@ -0,0 +1,10 @@
+# cook your dish here
+import math
+t = int(input())
+for i in range(t):
+    n = int(input())
+    if 500%n == 0:
+        print(500//n)
+    else:
+        print((500//n)+1)
+    
